5386143a91dfd856d21ddda04fad25fee5d1308e
Kristian Ullrich <ullrich@evolbio.mpg.de>
2021-01-22 20:58:26 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,253 @@
+[![License: GPLv3](https://img.shields.io/badge/License-GPL%20v3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
+[![Language: C99](https://img.shields.io/badge/Language-C99-orangered.svg)](https://en.wikipedia.org/wiki/C99)
+
 # literal-dists
+
 Convert a FASTA alignment with ambiguous nucleotides to literal distance matrix
+
+Accepts `A, C, G, T, R, Y, S, W, K, M, B, D, H, V, -, X, ., N`
+
+Forked and adapted from [snp-dists](https://github.com/tseemann/snp-dists)
+
+Almost all source code is taken from [snp-dists](https://github.com/tseemann/snp-dists),
+despite the literal-distance calculation
+
+Literal distances are implemented as mentioned by [Chang et al., 2017](https://pubmed.ncbi.nlm.nih.gov/28819774/)
+
+## Quick Start
+
+```
+% cat test/good.aln
+
+>seq1
+AGTCAGTC
+>seq2
+AGGCAGTC
+>seq3
+AGTGAGTA
+>seq4
+TGTTAGAC
+>seq5
+TGTCAYGR
+
+% literal-dists test/good.aln > distances.tab
+
+Read 4 sequences of length 8
+
+% cat distances.tab
+
+literal-dists 0.0.1	seq1	seq2	seq3	seq4	seq5
+seq1	0.000000	0.125000	0.250000	0.375000	0.500000
+seq2	0.125000	0.000000	0.375000	0.500000	0.625000
+seq3	0.250000	0.375000	0.000000	0.500000	0.562500
+seq4	0.375000	0.500000	0.500000	0.000000	0.500000
+seq5    0.500000	0.625000	0.562500	0.500000	0.000000
+```
+
+## Installation
+
+As `literal-dists` source code is almost completely taken from [snp-dists](https://github.com/tseemann/snp-dists)
+
+`literal-dists` is written in C to the C99 standard and only depends on `zlib`.
+
+### Source
+
+```
+git clone https://github.com/kullrich/literal-dists.git
+cd literal-dists
+make
+
+# run tests
+make check
+
+# optionally install to a specific location (default: /usr/local)
+make PREFIX=/usr/local install
+```
+
+## Options
+
+### `literal-dists -h` (help)
+
+```
+SYNOPSIS
+  Pairwise literal distance matrix from a FASTA alignment
+USAGE
+  literal-dists [options] alignment.fasta[.gz] > matrix.tsv
+OPTIONS
+  -h    Show this help
+  -v    Print version and exit
+  -t    Use original snp-dists distance
+  -q    Quiet mode; do not print progress information
+  -a    Count all differences not just [AGTC]
+  -k    Keep case, don't uppercase all letters
+  -m    Output MOLTEN instead of TSV
+  -c    Use comma instead of tab in output
+  -b    Blank top left corner cell
+URL
+  https://github.com/kullrich/literal-dists
+```
+
+### `literal-dists -v` (version)
+
+Prints the name and version separated by a space in standard Unix fashion.
+
+```
+literal-dists 0.0.1
+```
+
+### `literal-dists -q` (quiet mode)
+
+Don't print informational messages, only errors.
+
+### `literal-dists -c` (CSV instead of TSV)
+
+```
+literal-dists 0.0.1,seq1,seq2,seq3,seq4,seq5
+seq1,0.000000,0.125000,0.250000,0.375000,0.500000
+seq2,0.125000,0.000000,0.375000,0.500000,0.625000
+seq3,0.250000,0.375000,0.000000,0.500000,0.562500
+seq4,0.375000,0.500000,0.500000,0.000000,0.500000
+seq5,0.500000,0.625000,0.562500,0.500000,0.000000
+```
+
+### `literal-dists -b` (omit the toolname/version)
+
+```
+	seq1	seq2	seq3	seq4	seq5
+seq1	0.000000	0.125000	0.250000	0.375000	0.500000
+seq2	0.125000	0.000000	0.375000	0.500000	0.625000
+seq3	0.250000	0.375000	0.000000	0.500000	0.562500
+seq4	0.375000	0.500000	0.500000	0.000000	0.500000
+seq5	0.500000	0.625000	0.562500	0.500000	0.000000
+```
+
+### `literal-dists -t` (original snp-dists v0.7.0 distance calculation)
+
+```
+	seq1	seq2	seq3	seq4	seq5
+seq1	0	1	2	3	2
+seq2	1	0	3	4	3
+seq3	2	3	0	4	3
+seq4	3	4	4	0	2
+seq5	2	3	3	2	0
+```
+
+## Advanced options (for original snp-dists implementation only)
+
+By default, all letters are (1) uppercased and (2) ignored if not A, C, G, T.
+
+### `literal-dists -a` (don't just count AGTC)
+
+Normally one would not want to count ambiguous letters and gaps as a "difference"
+but if you desire, you can enable this option.
+
+```
+>seq1
+NGTCAGTC
+>seq2
+AG-CAGTC
+>seq3
+AGTGNGTA
+```
+
+### `literal-dists -k` (don't uppercase any letters)
+
+You may wish to preserve case, as you may wish lower-case characters
+to be masked in the comparison.
+```
+>seq1
+AgTCAgTC
+>seq2
+AggCAgTC
+>seq3
+AgTgAgTA
+```
+
+### `literal-dists -m` ("molten" output format)
+
+`seq1    seq2   dist    score   nsites`
+
+```
+seq1	seq1	0.000000	0.000000	8.000000
+seq1	seq2	0.125000	1.000000	8.000000
+seq1	seq3	0.250000	2.000000	8.000000
+seq1	seq4	0.375000	3.000000	8.000000
+seq1	seq5	0.500000	4.000000	8.000000
+seq2	seq1	0.125000	1.000000	8.000000
+seq2	seq2	0.000000	0.000000	8.000000
+seq2	seq3	0.375000	3.000000	8.000000
+seq2	seq4	0.500000	4.000000	8.000000
+seq2	seq5	0.625000	5.000000	8.000000
+seq3	seq1	0.250000	2.000000	8.000000
+seq3	seq2	0.375000	3.000000	8.000000
+seq3	seq3	0.000000	0.000000	8.000000
+seq3	seq4	0.500000	4.000000	8.000000
+seq3	seq5	0.562500	4.500000	8.000000
+seq4	seq1	0.375000	3.000000	8.000000
+seq4	seq2	0.500000	4.000000	8.000000
+seq4	seq3	0.500000	4.000000	8.000000
+seq4	seq4	0.000000	0.000000	8.000000
+seq4	seq5	0.500000	4.000000	8.000000
+seq5	seq1	0.500000	4.000000	8.000000
+seq5	seq2	0.625000	5.000000	8.000000
+seq5	seq3	0.562500	4.500000	8.000000
+seq5	seq4	0.500000	4.000000	8.000000
+seq5	seq5	0.000000	0.000000	8.000000
+```
+
+`literal-dists test/ambig.aln`
+
+```
+seq1	seq1	0.000000	0.000000	6.000000
+seq1	seq2	0.000000	0.000000	4.000000
+seq1	seq3	0.500000	2.000000	4.000000
+seq1	seq4	0.250000	1.000000	4.000000
+seq1	seq5	0.250000	1.000000	4.000000
+seq2	seq1	0.000000	0.000000	4.000000
+seq2	seq2	0.000000	0.000000	6.000000
+seq2	seq3	0.500000	2.000000	4.000000
+seq2	seq4	0.500000	2.000000	4.000000
+seq2	seq5	0.500000	2.000000	4.000000
+seq3	seq1	0.500000	2.000000	4.000000
+seq3	seq2	0.500000	2.000000	4.000000
+seq3	seq3	0.000000	0.000000	6.000000
+seq3	seq4	0.750000	3.000000	4.000000
+seq3	seq5	0.750000	3.000000	4.000000
+seq4	seq1	0.250000	1.000000	4.000000
+seq4	seq2	0.500000	2.000000	4.000000
+seq4	seq3	0.750000	3.000000	4.000000
+seq4	seq4	0.000000	0.000000	6.000000
+seq4	seq5	0.000000	0.000000	6.000000
+seq5	seq1	0.250000	1.000000	4.000000
+seq5	seq2	0.500000	2.000000	4.000000
+seq5	seq3	0.750000	3.000000	4.000000
+seq5	seq4	0.000000	0.000000	6.000000
+seq5	seq5	0.000000	0.000000	6.000000
+```
+
+
+## Issues
+
+Report bugs and give suggesions on the
+[Issues page](https://github.com/kullrich/literal-dists/issues)
+
+## Related software
+
+* [snp-dists](https://github.com/tseemann/snp-dists)
+* [Disty](https://github.com/c2-d2/disty)
+* [Panito](https://github.com/sanger-pathogens/panito)
+* [pairwise_snp_differences](https://github.com/MDU-PHL/pairwise_snp_differences/blob/master/pairwise_snp_differences.Rmd)
+
+## Licence
+
+[GPL Version 3](https://raw.githubusercontent.com/kullrich/literal-dists/master/LICENSE)
+
+## Authors
+
+* [Kristian Ullrich](https://github.com/kullrich)
+
+## References
+
+[https://github.com/tseemann/snp-dists](https://github.com/tseemann/snp-dists)
+
+Chang PL., Kopania E., Keeble S., Sarver BAJ., Larson E., Orth A., Belkhir K., Boursot P., Bonhomme F., Good JM., and Dean MW. (2017). **Whole exome sequencing of wild-derived inbred strains of mice improves power to link phenotype and genotype.** *Mammalian Genome*, **28**, 416-425. [https://dx.doi.org/10.1007%2Fs00335-017-9704-9](https://dx.doi.org/10.1007%2Fs00335-017-9704-9)
---
+++ main.c
@@ -0,0 +1,307 @@
+#include "kseq.h"
+#include <getopt.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <zlib.h>
+#include <string.h>
+
+#define VERSION "0.0.1"
+#define EXENAME "literal-dists"
+#define GITHUB_URL "https://github.com/kullrich/literal-dists"
+
+const int MAX_SEQ = 100000;
+const char IGNORE_CHAR = '.';
+const int PHRED = 33;
+
+// define score matrix
+char *nucnuc[324] = {
+  "AA", "AC", "AG", "AT", "AR", "AY", "AS", "AW", "AK", "AM", "AB", "AD", "AH", "AV", "A.", "A-", "AN", "AX",
+  "CA", "CC", "CG", "CT", "CR", "CY", "CS", "CW", "CK", "CM", "CB", "CD", "CH", "CV", "C.", "C-", "CN", "CX",
+  "GA", "GC", "GG", "GT", "GR", "GY", "GS", "GW", "GK", "GM", "GB", "GD", "GH", "GV", "G.", "G-", "GN", "GX",
+  "TA", "TC", "TG", "TT", "TR", "TY", "TS", "TW", "TK", "TM", "TB", "TD", "TH", "TV", "T.", "T-", "TN", "TX",
+  "RA", "RC", "RG", "RT", "RR", "RY", "RS", "RW", "RK", "RM", "RB", "RD", "RH", "RV", "R.", "R-", "RN", "RX",
+  "YA", "YC", "YG", "YT", "YR", "YY", "YS", "YW", "YK", "YM", "YB", "YD", "YH", "YV", "Y.", "Y-", "YN", "YX",
+  "SA", "SC", "SG", "ST", "SR", "SY", "SS", "SW", "SK", "SM", "SB", "SD", "SH", "SV", "S.", "S-", "SN", "SX",
+  "WA", "WC", "WG", "WT", "WR", "WY", "WS", "WW", "WK", "WM", "WB", "WD", "WH", "WV", "W.", "W-", "WN", "WX",
+  "KA", "KC", "KG", "KT", "KR", "KY", "KS", "KW", "KK", "KM", "KB", "KD", "KH", "KV", "K.", "K-", "KN", "KX",
+  "MA", "MC", "MG", "MT", "MR", "MY", "MS", "MW", "MK", "MM", "MB", "MD", "MH", "MV", "M.", "M-", "MN", "MX",
+  "BA", "BC", "BG", "BT", "BR", "BY", "BS", "BW", "BK", "BM", "BB", "BD", "BH", "BV", "B.", "B-", "BN", "BX",
+  "DA", "DC", "DG", "DT", "DR", "DY", "DS", "DW", "DK", "DM", "DB", "DD", "DH", "DV", "D.", "D-", "DN", "DX",
+  "HA", "HC", "HG", "HT", "HR", "HY", "HS", "HW", "HK", "HM", "HB", "HD", "HH", "HV", "H.", "H-", "HN", "HX",
+  "VA", "VC", "VG", "VT", "VR", "VY", "VS", "VW", "VK", "VM", "VB", "VD", "VH", "VV", "V.", "V-", "VN", "VX",
+  ".A", ".C", ".G", ".T", ".R", ".Y", ".S", ".W", ".K", ".M", ".B", ".D", ".H", ".V", "..", ".-", ".N", ".X",
+  "-A", "-C", "-G", "-T", "-R", "-Y", "-S", "-W", "-K", "-M", "-B", "-D", "-H", "-V", "-.", "--", "-N", "-X",
+  "NA", "NC", "NG", "NT", "NR", "NY", "NS", "NW", "NK", "NM", "NB", "ND", "NH", "NV", "N.", "N-", "NN", "NX",
+  "XA", "XC", "XG", "XT", "XR", "XY", "XS", "XW", "XK", "XM", "XB", "XD", "XH", "XV", "X.", "X-", "XN", "XX"
+};
+double nucnucscore[324] = {
+  0.0, 1.0, 1.0, 1.0, 0.5, 1.0, 1.0, 0.5, 1.0, 0.5, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0,
+  1.0, 0.0, 1.0, 1.0, 1.0, 0.5, 0.5, 1.0, 1.0, 0.5, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0,
+  1.0, 1.0, 0.0, 1.0, 0.5, 1.0, 0.5, 1.0, 0.5, 1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0,
+  1.0, 1.0, 1.0, 0.0, 1.0, 0.5, 1.0, 0.5, 0.5, 1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0,
+  0.5, 1.0, 0.5, 1.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0,
+  1.0, 0.5, 1.0, 0.5, 1.0, 0.0, 1.0, 1.0, 1.0, 1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0,
+  1.0, 0.5, 0.5, 1.0, 1.0, 1.0, 0.0, 1.0, 1.0, 1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0,
+  0.5, 1.0, 1.0, 0.5, 1.0, 1.0, 1.0, 0.0, 1.0, 1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0,
+  1.0, 1.0, 0.5, 0.5, 1.0, 1.0, 1.0, 1.0, 0.0, 1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0,
+  0.5, 0.5, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0,
+  -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0,
+  -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0,
+  -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0,
+  -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0,
+  -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0,
+  -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0,
+  -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0,
+  -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0
+};
+
+KSEQ_INIT(gzFile, gzread)
+
+//------------------------------------------------------------------------
+size_t distance(const char* restrict a, const char* restrict b, size_t L)
+{
+  size_t diff=0;
+  for (size_t i=0; i < L; i++) {
+    if (a[i] != b[i] && a[i] != IGNORE_CHAR && b[i] != IGNORE_CHAR) {
+      diff++;
+    }
+  }
+  return diff;
+}
+
+void literaldistance(const char* restrict a, const char* restrict b, size_t L, double scorematrix[][57], double about[])
+{
+  double abdist=0.0;
+  double abnsites=0.0;
+  double abscore=0.0;
+  double abscoreij=0.0;
+  int ai, bi;
+  abnsites = L;
+  for (size_t i=0; i < L; i++) {
+    if (a[i] != IGNORE_CHAR && b[i] != IGNORE_CHAR) {
+        ai = a[i];
+        bi = b[i];
+        abscoreij=scorematrix[ai-PHRED][bi-PHRED];
+        //printf("i: %zu a[i]: %c %d b[i]: %c %d score: %f\n", i, a[i], ai, b[i], bi, abscore);
+        if(abscoreij>=0){
+          //abnsites+=1.0;
+          abscore+=abscoreij;
+        } else{
+          abnsites-=1.0;
+        }
+    } else {
+      abnsites-=1.0;
+    }
+  }
+  abdist=abscore/abnsites;
+  //printf("abdist: %f abscore: %f abnsites: %f\n", abdist, abscore, abnsites);
+  about[0]=abdist;
+  about[1]=abscore;
+  about[2]=abnsites;
+}
+
+//------------------------------------------------------------------------
+void show_help(int retcode)
+{
+  FILE* out = (retcode == EXIT_SUCCESS ? stdout : stderr);
+
+  static const char str[] = {
+      "SYNOPSIS\n  Pairwise literal distance matrix from a FASTA alignment\n"
+      "USAGE\n  %s [options] alignment.fasta[.gz] > matrix.tsv\n"
+      "OPTIONS\n"
+      "  -h\tShow this help\n"
+      "  -v\tPrint version and exit\n"
+      "  -t\tUse original snp-dists distance\n"
+      "  -q\tQuiet mode; do not print progress information\n"
+      "  -a\tCount all differences not just [AGTC]\n"
+      "  -k\tKeep case, don't uppercase all letters\n"
+      "  -m\tOutput MOLTEN instead of TSV\n"
+      "  -c\tUse comma instead of tab in output\n"
+      "  -b\tBlank top left corner cell\n"
+      "URL\n  %s\n"};
+  fprintf(out, str, EXENAME, GITHUB_URL);
+  exit(retcode);
+}
+
+//------------------------------------------------------------------------
+int main(int argc, char* argv[])
+{
+  // parse command line parameters
+  int opt, quiet = 0, csv = 0, corner = 1, allchars = 0, keepcase = 0, molten = 0, snpdists = 0;
+  while ((opt = getopt(argc, argv, "htqcakmbv")) != -1) {
+    switch (opt) {
+      case 'h': show_help(EXIT_SUCCESS); break;
+      case 't': snpdists = 1; break;
+      case 'q': quiet = 1; break;
+      case 'c': csv = 1; break;
+      case 'a': allchars = 1; break;
+      case 'k': keepcase = 1; break;
+      case 'm': molten = 1; break;
+      case 'b': corner = 0; break;
+      case 'v': printf("%s %s\n", EXENAME, VERSION); exit(EXIT_SUCCESS);
+      default: show_help(EXIT_FAILURE);
+    }
+  }
+
+  // require a filename argument
+  if (optind >= argc) {
+    show_help(EXIT_FAILURE);
+    return 0;
+  }
+  const char* fasta = argv[optind];
+
+  // say hello
+  if (!quiet)
+    fprintf(stderr, "This is %s %s\n", EXENAME, VERSION);
+
+  // open filename via libz
+  gzFile fp = gzopen(fasta, "r");
+  if (!fp) {
+    fprintf(stderr, "ERROR: Could not open filename '%s'\n", fasta);
+    exit(EXIT_FAILURE);
+  }
+
+  // load all the sequences
+  char** seq = calloc(MAX_SEQ, sizeof(char*));
+  char** name = calloc(MAX_SEQ, sizeof(char*));
+  ssize_t l, N=0, L=-1;
+  kseq_t* kseq = kseq_init(fp);
+
+  while ((l = kseq_read(kseq)) >= 0) {
+    // first sequence
+    if (L < 0) {
+      L = l;
+    }
+    // not first sequence - so ensure length matches first one
+    if (l != L) {
+      fprintf(stderr,
+              "ERROR: sequence #%zu '%s' has length %zu but expected %zu\n",
+              N + 1, kseq->name.s, l, L);
+      exit(EXIT_FAILURE);
+    }
+    // have we exceeded the number of sequences we can handle?
+    if (N >= MAX_SEQ) {
+      fprintf(stderr,
+              "ERROR: %s can only handle %d sequences at most. Please change MAX_SEQ "
+              "and recompile.\n",
+              EXENAME, MAX_SEQ);
+      exit(EXIT_FAILURE);
+    }
+    // save the sequence and name
+    seq[N] = (char*)calloc(kseq->seq.l + 1, sizeof(char));
+    strcpy(seq[N], kseq->seq.s);
+    name[N] = (char*)calloc(kseq->name.l + 1, sizeof(char));
+    strcpy(name[N], kseq->name.s);
+
+    // uppercase all sequences
+    if (!keepcase) {
+      for (char* s = seq[N]; *s; s++) {
+        *s = toupper(*s);
+      }
+    }
+
+    // clean the sequence depending on -a option
+    if (!allchars) {
+      for (char* s = seq[N]; *s; s++) {
+        if(snpdists){
+          if (*s != 'A' && *s != 'T' && *s != 'C' && *s != 'G') {
+            *s = IGNORE_CHAR;
+          }
+        } else {
+          if (*s != 'A' && *s != 'C' && *s != 'G' && *s != 'T' && *s != 'R' && *s != 'Y' && *s != 'S' && *s != 'W' && *s != 'K' && *s != 'M' && *s != 'B' && *s != 'D' && *s != 'H' && *s != 'V' && *s != '-' && *s != 'X' && *s != 'N') {
+            *s = IGNORE_CHAR;
+          }
+        }
+      }
+    }
+
+    // keep track of how many we have
+    N++;
+  }
+  kseq_destroy(kseq);
+  gzclose(fp);
+
+  if (N < 1) {
+    fprintf(stderr, "ERROR: file contained no sequences\n");
+    exit(EXIT_FAILURE);
+  }
+
+  if (!quiet)
+    fprintf(stderr, "Read %zu sequences of length %zu\n", N, L);
+
+  // output TSV or CSV
+  char sep = csv ? ',' : '\t';
+
+  // create score matrix
+  double scorematrix[57][57] = {{0.0}};
+  if (!snpdists) {
+    for (int i=0;i < 324;i++){
+        //printf("i: %d %d %c %d %c %f\n", i, nucnuc[i][0]-PHRED, nucnuc[i][0], nucnuc[i][1]-PHRED, nucnuc[i][1], nucnucscore[i]);
+        scorematrix[nucnuc[i][0]-PHRED][nucnuc[i][1]-PHRED]=nucnucscore[i];
+    }
+  }
+
+  if (molten) {
+    // "molten" format, one row per pair
+    for (int j = 0; j < N; j++) {
+      for (int i=0; i < N; i++) {
+        if(snpdists){
+            size_t d = distance(seq[j], seq[i], L);
+            printf("%s%c%s%c%zu\n", name[j], sep, name[i], sep, d);
+        } else{
+          double about[3];
+          literaldistance(seq[j], seq[i], L, scorematrix, about);
+          //printf("abdist: %f abscore: %f abnsites: %f\n", about[0], about[1], about[2]);
+          printf("%s%c%s%c%f%c%f%c%f\n", name[j], sep, name[i], sep, about[0], sep, about[1], sep, about[2]);
+        }
+      }
+    }
+  }
+  else {
+    // regular TSV matrix output
+
+    // header seq
+    if (corner){
+      if(snpdists){
+        printf("snp-dists 0.7.0");
+      } else{
+        printf("%s %s", EXENAME, VERSION);
+      }
+    }
+    for (int j = 0; j < N; j++) {
+      printf("%c%s", sep, name[j]);
+    }
+    printf("\n");
+
+    // Output the distance matrix to stdout
+    // (does full matrix, wasted computation i know)
+    for (int j = 0; j < N; j++) {
+      printf("%s", name[j]);
+      for (int i=0; i < N; i++) {
+        if(snpdists){
+          size_t d = distance(seq[j], seq[i], L);
+          printf("%c%zu", sep, d);
+        } else{
+          double about[3];
+          literaldistance(seq[j], seq[i], L, scorematrix, about);
+          printf("%c%f", sep, about[0]);
+        }
+      }
+      printf("\n");
+    }
+  }
+
+  // free memory
+  for (int k = 0; k < N; k++) {
+    free(seq[k]);
+    free(name[k]);
+  }
+  free(seq);
+  free(name);
+
+  return 0;
+}
+
+//------------------------------------------------------------------------
