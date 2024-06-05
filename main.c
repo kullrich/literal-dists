ad114229553b8cfd9d9d0dac40c52f86492bf9db
Kristian Ullrich <ullrich@evolbio.mpg.de>
2024-06-05 16:47:00 +0200

    Update main.c

--- main.c
+++ main.c
@@ -7,7 +7,7 @@
 #include <zlib.h>
 #include <string.h>
 
-#define VERSION "0.0.2"
+#define VERSION "0.0.3"
 #define EXENAME "literal-dists"
 #define GITHUB_URL "https://github.com/kullrich/literal-dists"
 
@@ -124,7 +124,9 @@ void show_help(int retcode)
   
   static const char str[] = {
       "SYNOPSIS\n  Pairwise literal distance matrix from a FASTA alignment\n"
-      "USAGE\n  %s [options] alignment.fasta[.gz] > matrix.tsv\n"
+      "USAGE\n  %s [options] -f alignment.fasta[.gz] > matrix.tsv\n"
+      "or\n"
+      "USAGE\n  %s [options] < alignment.fasta[.gz] > matrix.tsv\n"
       "OPTIONS\n"
       "  -h\tShow this help\n"
       "  -v\tPrint version and exit\n"
@@ -138,6 +140,7 @@ void show_help(int retcode)
       "  -i\tOutput used sites, scores and gap sites to TSV\n"
       "  -g\tSkip gap sites if gap frequency is met, gap sites [.-NX]\n"
       "  -z\tGap frequency [default: 0.5]\n"
+      "  -f\tInput FASTA file\n"
       "URL\n  %s\n"};
   fprintf(out, str, EXENAME, GITHUB_URL);
   exit(retcode);
@@ -150,7 +153,7 @@ int main(int argc, char* argv[])
   int opt, quiet = 0, csv = 0, corner = 1, sitestotsv = 0, allchars = 0, remgaps = 0, keepcase = 0, molten = 0, snpdists = 0;
   double gapfreq = 0.5;
   int gapsites = 0;
-  while ((opt = getopt(argc, argv, "htqcakmbigz:v")) != -1) {
+  while ((opt = getopt(argc, argv, "htqcakmbigz:vf")) != -1) {
     switch (opt) {
       case 'h': show_help(EXIT_SUCCESS); break;
       case 't': snpdists = 1; break;
@@ -164,24 +167,33 @@ int main(int argc, char* argv[])
       case 'g': remgaps = 1; break;
       case 'z': gapfreq = strtod(optarg, NULL); break;
       case 'v': printf("%s %s\n", EXENAME, VERSION); exit(EXIT_SUCCESS);
+      case 'f': fasta_file = 1; break;
       default: show_help(EXIT_FAILURE);
     }
   }
-  // require a filename argument
-  if (optind >= argc) {
-    show_help(EXIT_FAILURE);
-    return 0;
-  }
-  const char* fasta = argv[optind];
   // say hello
   if (!quiet) {
     fprintf(stderr, "This is %s %s\n", EXENAME, VERSION);
   }
   // open filename via libz
-  gzFile fp = gzopen(fasta, "r");
-  if (!fp) {
-    fprintf(stderr, "ERROR: Could not open filename '%s'\n", fasta);
-    exit(EXIT_FAILURE);
+  gzFile fp;
+  if (fasta_file) {
+    if (optind >= argc) {
+      show_help(EXIT_FAILURE);
+      return 0;
+    }
+    const char* fasta = argv[optind];
+    fp = gzopen(fasta, "r");
+    if (!fp) {
+      fprintf(stderr, "ERROR: Could not open filename '%s'\n", fasta);
+      exit(EXIT_FAILURE);
+    }
+  } else {
+    fp = gzdopen(fileno(stdin), "r");
+    if (!fp) {
+      fprintf(stderr, "ERROR: Could not open stdin\n");
+      exit(EXIT_FAILURE);
+    }
   }
   // load all the sequences
   char** seq = calloc(MAX_SEQ, sizeof(char*));
